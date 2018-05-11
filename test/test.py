8862d7119855b6e6ff339125db868ea02c4c19c0
Paweł Karwacki <pawel.witcher@wp.pl>
2018-05-11 12:12:14 +0200

    Test new NLP functionalities

--- test/test.py
+++ test/test.py
@@ -1,83 +1,81 @@
 import json
-import links_from_header
 import nltk
-import time
 import pickle
 import re
-import requests
+from bs4 import BeautifulSoup
+from gensim import corpora
+from gensim import models
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-from requests.auth import HTTPBasicAuth
-from test import config
+from markdown2 import markdown
 
 
-def safe_request(link):
-    auth = HTTPBasicAuth(config.login, config.password)
-    while True:
-        try:
-            response = requests.get(link, auth=auth)
-            if response.status_code == 200 or response.status_code == 400:
-                break
-        except requests.exceptions.ConnectionError:
-            print('Waiting 10 seconds...')
-            print(link)
-            time.sleep(10)
-            continue
-        print('Waiting 10 seconds...')
-        print(link)
-        time.sleep(10)
-    return response
+def extract_tokens_from_markdown(document):
+    html = markdown(document)
+    text = BeautifulSoup(html, 'html5lib').findAll(text=True)
+    text = "".join(text)
+    text = text.replace("\\n", " ")
 
-
-def find_files(repo, pattern):
-    result = []
-    download_prefix = 'https://raw.githubusercontent.com/'
-    file_id_regex = re.compile('.*blob(.*)\\b')
-    link = 'https://api.github.com/search/code?q=' + pattern + '%20in:path+repo:' + repo
-    response = safe_request(link)
-    for element in json.loads(response.content)['items']:
-        file_id = file_id_regex.search(element['html_url']).group(1)
-        link = download_prefix + repo + file_id
-        file = safe_request(link)
-        result.append(file.content)
-
-    return result
-
-
-def tokenize(document, re_filter, stop_words):
     st = PorterStemmer()
-    try:
-        tokens = nltk.word_tokenize(document.decode('utf-8'))
-    except UnicodeDecodeError:
-        return []
-    tokens = [t.lower() for t in tokens]
-    tokens = [st.stem(t) for t in tokens if t not in stop_words and re_filter.match(t)]
+    tokens = nltk.word_tokenize(text)
+    tokens = [token for token in tokens if re.search('^([a-zA-Z]+[\w-]*\w+)$', token)]
+    tokens = [token.lower() for token in tokens]
+    tokens = [token for token in tokens if token not in set(stopwords.words('english'))]
+    tokens = [st.stem(token) for token in tokens]
+
     return tokens
 
 
-def github_corpus_generator():
-    link = 'https://api.github.com/search/repositories?q=stars:%3E=8000'
-    re_filter = re.compile('[a-zA-Z]+')
-    stop_words = set(stopwords.words('english'))
-
-    while True:
-        response = safe_request(link)
-        for repo in json.loads(response.content)['items']:
-            readme_files = find_files(repo['full_name'], 'readme')
-            for document in readme_files:
-                result = tokenize(document, re_filter, stop_words)
-                if len(result) > 0:
-                    yield result
-
-        links = links_from_header.extract(response.headers['link'])
-        if 'next' not in links.keys():
-            break
-        link = links['next']
-        print('Next page...')
-
-
 if __name__ == '__main__':
-    with open('texts.txt', 'wb') as f:
-        for text in github_corpus_generator():
-            print(text)
-            pickle.dump(text, f)
+    # Dictionary and tokens
+    # with open('../gh-database/page1.json', 'r') as f:
+    #     file = json.loads(f.read())
+    #     readme_tokens = []
+    #     for repo in file:
+    #         print('Processing ' + repo['name'] + 'repository...')
+    #         readme = repo['readme']
+    #         readme_tokens.append(extract_tokens_from_markdown(readme))
+    #     with open('../resources/tokens/tokens1.tk', 'wb') as tokens_file:
+    #         pickle.dump(readme_tokens, tokens_file)
+    #
+    #     dictionary = corpora.Dictionary()
+    #     dictionary.add_documents(readme_tokens)
+    #     dictionary.filter_extremes(no_below=0, no_above=0.5, keep_n=None)
+    #     dictionary.save('../resources/bow/dictionary1.dict')
+
+    # BoW corpus
+    # readme_tokens = 0
+    # dictionary = corpora.Dictionary.load('../resources/bow/dictionary1.dict')
+    # with open('../resources/tokens/tokens1.tk', 'rb') as f:
+    #     readme_tokens = pickle.load(f)
+    # bows = [dictionary.doc2bow(token) for token in readme_tokens]
+    # with open('../resources/bow/bows1.bow', 'wb') as bows_file:
+    #     pickle.dump(bows, bows_file)
+
+    # Tfidf
+    # corpus = 0
+    # with open('../resources/bow/bows1.bow', 'rb') as f:
+    #     corpus = pickle.load(f)
+    # tfidf = models.TfidfModel(corpus)
+    # tfidf.save('../resources/tfidf/model.tfidf')
+
+    # Tfidf corpus
+    # tfidf = models.TfidfModel.load('../resources/tfidf/model.tfidf')
+    # corpus = 0
+    # with open('../resources/bow/bows1.bow', 'rb') as f:
+    #     corpus = pickle.load(f)
+    # corpus_tfidf = tfidf[corpus]
+    # with open('../resources/tfidf/corpus1.tfidf', 'wb') as f:
+    #     pickle.dump(corpus_tfidf, f)
+
+    # Lsi
+    corpus_tfidf = 0
+    with open('../resources/tfidf/corpus1.tfidf', 'rb') as f:
+        corpus_tfidf = pickle.load(f)
+    dictionary = corpora.Dictionary.load('../resources/bow/dictionary1.dict')
+    lsi = models.LsiModel(corpus=corpus_tfidf, id2word=dictionary, num_topics=300)
+    lsi.save('../resources/lsi/model.lsi')
+
+    # Lsi corpus and indexing
+
+    # Search
